c79044db4a49336d767a901c6b4d334b3da4538a
gpittarelli <tf2stadium@gjp.cc>
2015-11-04 13:22:06 -0500

    code cleanup

--- wsevent.js
+++ wsevent.js
@@ -5,53 +5,54 @@ function Socket(conn, extractor) {
     return new Socket(conn, extractor);
   }
 
-	this.conn = conn;
-	this.replyHandlers = [];
-	this.eventHandlers = [];
-	this.extractor = extractor
-	var self = this;
-	conn.onmessage = function (event) {
-	    try {
-	    var json = JSON.parse(event.data);
-	    }
-	    catch(SyntaxError) {
-		console.alert("Syntax error in received data");
-		return;
-	    }
+  this.conn = conn;
+  this.replyHandlers = [];
+  this.eventHandlers = [];
+  this.extractor = extractor;
 
-	    if (json.data == undefined) {
-		console.log("No data field in received data");
-		return;
-	    }
+  var self = this;
+  conn.onmessage = function (event) {
+    try {
+      var json = JSON.parse(event.data);
+    }
+    catch(SyntaxError) {
+      console.alert("Syntax error in received data");
+      return;
+    }
 
-	    if (typeof json['id'] == 'string') {
-		var id = json['id'];
+    if (json.data == undefined) {
+      console.log("No data field in received data");
+      return;
+    }
 
-		var callback = self.replyHandlers[id]
-		callback(json.data)
+    var callback;
+    if (typeof json['id'] == 'string') {
+      var id = json['id'];
 
-		var index = self.replyHandlers.indexOf(id);
-		if (index > -1) {
-		    self.replyHandlers.splice(index, 1);
-		}
+      callback = self.replyHandlers[id];
+      callback(json.data);
 
-	    } else {
+      var index = self.replyHandlers.indexOf(id);
+      if (index > -1) {
+        self.replyHandlers.splice(index, 1);
+      }
 
-		var callback = self.eventHandlers[self.extractor(json.data)];
-		if (callback != undefined) {
-		    callback(JSON.parse(json.data.data));
-		}
-	    }
-	};
+    } else {
+      callback = self.eventHandlers[self.extractor(json.data)];
+      if (callback != undefined) {
+        callback(JSON.parse(json.data.data));
+      }
+    }
+  };
 
-	this.On = function (e, callback) {
-	    this.eventHandlers[e] = callback;
-	}
+  this.On = function (e, callback) {
+    this.eventHandlers[e] = callback;
+  };
 
-	this.Emit = function (data, callback) {
-	    var id = asmCrypto.SHA1.hex(""+(Date.now() ^ Math.random()*1000))
-	    var json = {id: id, data: data};
-	    this.replyHandlers[id] = callback;
-	    this.conn.send(JSON.stringify(json));
-	}
+  this.Emit = function (data, callback) {
+    var id = asmCrypto.SHA1.hex(""+(Date.now() ^ Math.random()*1000));
+    var json = {id: id, data: data};
+    this.replyHandlers[id] = callback;
+    this.conn.send(JSON.stringify(json));
+  };
 }
