8beed73b5b57f3f1f8249a09030ce19376637107
gpittarelli <tf2stadium@gjp.cc>
2015-11-04 19:54:09 -0500

    Rewrite to be jshint-clean javacsript

--- wsevent.js
+++ wsevent.js
@@ -1,58 +1,71 @@
-'use strict';
+(function (window) {
+  'use strict';
 
-function Socket(conn, extractor) {
-  if (!(this instanceof Socket)) {
-    return new Socket(conn, extractor);
+  function Socket(conn, extractor) {
+    if (!(this instanceof Socket)) {
+      throw new TypeError('Cannot call a class as a function');
+    }
+
+    if (typeof conn === 'string') {
+      this.conn = new WebSocket(conn);
+    } else if (conn instanceof WebSocket) {
+      this.conn = conn;
+    } else {
+      throw new TypeError('Socket: first arg must be a string or WebSocket');
+    }
+
+    this.replyHandlers = Object.create(null);
+    this.eventHandlers = Object.create(null);
+
+    this.extractor = extractor;
+
+    var self = this;
+    conn.onmessage = function (event) {
+      var json, callback;
+
+      try {
+        json = JSON.parse(event.data);
+      }
+      catch(SyntaxError) {
+        console.alert('Syntax error in received data');
+        return;
+      }
+
+      if (json.data === undefined) {
+        console.log('No data field in received data');
+        return;
+      }
+
+      if (typeof json.id === 'string') {
+        // Handle response messages
+        var id = json.id;
+        callback = self.replyHandlers[id];
+
+        if (callback) {
+          callback(JSON.parse(json.data));
+          delete self.replyHandlers[id];
+        }
+      } else {
+        callback = self.eventHandlers[self.extractor(json.data)];
+        if (callback !== undefined) {
+          // Note: messages from the server are an extra layer deep
+          // (so .data.data instead of the single .data of responses)
+          callback(JSON.parse(json.data.data));
+        }
+      }
+    };
   }
 
-  this.conn = conn;
-  this.replyHandlers = [];
-  this.eventHandlers = [];
-  this.extractor = extractor;
-
-  var self = this;
-  conn.onmessage = function (event) {
-    try {
-      var json = JSON.parse(event.data);
-    }
-    catch(SyntaxError) {
-      console.alert("Syntax error in received data");
-      return;
-    }
-
-    if (json.data == undefined) {
-      console.log("No data field in received data");
-      return;
-    }
-
-    var callback;
-    if (typeof json['id'] == 'string') {
-      var id = json['id'];
-
-      callback = self.replyHandlers[id];
-      callback(json.data);
-
-      var index = self.replyHandlers.indexOf(id);
-      if (index > -1) {
-        self.replyHandlers.splice(index, 1);
-      }
-
-    } else {
-      callback = self.eventHandlers[self.extractor(json.data)];
-      if (callback != undefined) {
-        callback(JSON.parse(json.data.data));
-      }
-    }
-  };
-
-  this.On = function (e, callback) {
+  Socket.prototype.On = function On(e, callback) {
     this.eventHandlers[e] = callback;
   };
 
-  this.Emit = function (data, callback) {
+  Socket.prototype.Emit = function Emit(data, callback) {
     var id = asmCrypto.SHA1.hex(""+(Date.now() ^ Math.random()*1000));
     var json = {id: id, data: data};
     this.replyHandlers[id] = callback;
     this.conn.send(JSON.stringify(json));
   };
-}
+
+  window.Socket = Socket;
+})(this);
