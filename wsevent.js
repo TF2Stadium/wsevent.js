7a2abf1b6e135cdd07d0f69d88607f4279182286
gpittarelli <tf2stadium@gjp.cc>
2015-11-06 00:34:56 -0500

    Handle socket reconnecting

--- wsevent.js
+++ wsevent.js
@@ -8,33 +8,115 @@
     };
   })();
 
-  function Socket(conn, extractor) {
-    if (!(this instanceof Socket)) {
-      throw new TypeError('Cannot call a class as a function');
+  function noop() {}
+
+  // Extend WebSocket to automatically reconnect
+  function ReconnectingWebSocket(url, opts) {
+    if (!(this instanceof ReconnectingWebSocket)) {
+      throw new TypeError('Cannot call a constructor as a function');
     }
 
-    if (typeof conn === 'string') {
-      this.conn = new WebSocket(conn);
-    } else if (conn instanceof WebSocket) {
-      this.conn = conn;
-    } else {
-      throw new TypeError('Socket: first arg must be a string or WebSocket');
+    opts = opts || {};
+
+    var self = this;
+    var timeout = opts.timeout || 100;
+    var maxRetries = opts.maxRetries || 5;
+    var curRetries = 0;
+
+    // External event callbacks
+    self.onmessage = noop;
+    self.onopen = noop;
+    self.onclose = noop;
+
+    function unreliableOnOpen(e) {
+      self.onopen(e);
+      curRetries = 0;
     }
+
+    function unreliableOnClose(e) {
+      self.onclose();
+
+      if (curRetries < maxRetries) {
+        ++curRetries;
+        setTimeout(connect, timeout);
+      }
+    }
+
+    function unreliableOnMessage(e) {
+      self.onmessage(e);
+    }
+
+    function connect() {
+      // Constructing a WebSocket() with opts.protocols === undefined
+      // does NOT behave the same as calling it with only one argument
+      // (specifically, it throws security errors).
+      if (opts.protocols) {
+        self.ws = new WebSocket(url, opts.protocols);
+      } else {
+        self.ws = new WebSocket(url);
+      }
+
+      // onerror isn't necessary: it is always accompanied by onclose
+      self.ws.onopen = unreliableOnOpen;
+      self.ws.onclose = unreliableOnClose;
+      self.ws.onmessage = unreliableOnMessage;
+    }
+
+    connect();
+  }
+
+  ReconnectingWebSocket.prototype.send = function send(data) {
+    this.ws.send(data);
+  };
+
+
+  function Socket(url, opts) {
+    if (!(this instanceof Socket)) {
+      throw new TypeError('Cannot call a constructor as a function');
+    }
+
+    if (typeof url === 'string') {
+      this.url = url;
+    } else {
+      throw new TypeError('Socket: first arg must be a string');
+    }
+
+    if (typeof opts !== 'object' || typeof opts.extractor !== 'function') {
+      throw new TypeError('Socket: opts must supply an extractor');
+    }
+
+    opts = opts || {};
+    var extractor = opts.extractor;
+
+    var self = this;
+
+    // External event callbacks
+    this.onopen = noop;
+    this.onclose = noop;
+    this.onmessage = noop;
+
+    this.conn = new ReconnectingWebSocket(url);
+    this.conn.onopen = function () {
+      self.onopen();
+    };
+    this.conn.onclose = function () {
+      self.onclose();
+    };
 
     this.replyHandlers = Object.create(null);
     this.eventHandlers = Object.create(null);
 
-    this.extractor = extractor;
-
-    var self = this;
-    conn.onmessage = function (event) {
+    this.conn.onmessage = function (e) {
       var json, callback;
 
+      // All
+      self.onmessage(e);
+
       try {
-        json = JSON.parse(event.data);
+        json = JSON.parse(e.data);
       }
       catch(SyntaxError) {
-        console.alert('Syntax error in received data');
+        console.log('Syntax error in received data');
         return;
       }
 
@@ -54,7 +136,7 @@
           delete self.replyHandlers[id];
         }
       } else {
-        callback = self.eventHandlers[self.extractor(json.data)];
+        callback = self.eventHandlers[extractor(json.data)];
         if (callback !== undefined) {
           // Note: messages from the server are an extra layer deep
           // (so .data.data instead of the single .data of responses)
